b946a7c6057cc1f5211ed4381b8f503a8e8945f7
Elok Mutiaraningtyas <elokmutiaraningtyas@gmail.com>
2025-05-20 20:06:08 +0700

    Update 584-Find-Customer-Referee.sql

--- 584-Find-Customer-Referee.sql
+++ 584-Find-Customer-Referee.sql
@@ -1,4 +1,53 @@
 --584. Find Customer Referee
-SELECT name
-FROM Customer
-WHERE referee_id != 2 or referee_id is NULL
+
+/*
+Description:
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Challenge: Find the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+*/
+
+-- Answer
+
+SELECT name -- select column
+FROM Customer -- from table name
+WHERE referee_id != 2 or referee_id is NULL -- where referee_id is not 2, since the data type are integer and "!= 2" only shows row with integer type, so we have to add "or referee_id is NULL" to also include null result
