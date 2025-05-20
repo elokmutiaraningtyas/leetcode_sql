c1448306944027148c0a51b40d3968649d4ae8c2
Elok Mutiaraningtyas <elokmutiaraningtyas@gmail.com>
2025-05-20 20:24:01 +0700

    Update 1683-Invalid-Tweets.sql

--- 1683-Invalid-Tweets.sql
+++ 1683-Invalid-Tweets.sql
@@ -1,11 +1,48 @@
 --1683. Invalid Tweets
-with cte as
-(
-SELECT tweet_id, content, LENGTH(content) as len
-from Tweets
-group by 1,2
-)
+/*
+Description:
+Table: Tweets
 
-select tweet_id
-from cte
-where len > 15
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of alphanumeric characters, '!', or ' ' and no other special characters.
+This table contains all the tweets in a social media app.
+ 
+Challenge:
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+Example 1:
+
+Input: 
+Tweets table:
++----------+-----------------------------------+
+| tweet_id | content                           |
++----------+-----------------------------------+
+| 1        | Let us Code                       |
+| 2        | More than fifteen chars are here! |
++----------+-----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+Explanation: 
+Tweet 1 has length = 11. It is a valid tweet.
+Tweet 2 has length = 33. It is an invalid tweet.
+*/
+
+-- Code & Explanations:
+select tweet_id -- select column name
+from Tweets -- from table name
+where LENGTH(content) > 15 -- where criteria of the "content" column have more than 15 character, you can use LENGTH or CHAR_LENGTH 
