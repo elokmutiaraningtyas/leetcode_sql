b39a5a7948c98714609ac181a9c43cf49519d506
Elok Mutiaraningtyas <elokmutiaraningtyas@gmail.com>
2025-05-20 20:34:08 +0700

    Update 1378-Replace-Employee-ID-With-The-Unique-Identifier.sql

--- 1378-Replace-Employee-ID-With-The-Unique-Identifier.sql
+++ 1378-Replace-Employee-ID-With-The-Unique-Identifier.sql
@@ -1,4 +1,76 @@
---1378. Replace Employee ID With The Unique Identifier
-select b.unique_id, a.name
-from Employees as a
-left join EmployeeUNI as b on b.id = a.id
+-- 1378. Replace Employee ID With The Unique Identifier
+/*
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+
+Challenge: Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Alice    |
+| 7  | Bob      |
+| 11 | Meir     |
+| 90 | Winston  |
+| 3  | Jonathan |
++----+----------+
+EmployeeUNI table:
++----+-----------+
+| id | unique_id |
++----+-----------+
+| 3  | 1         |
+| 11 | 2         |
+| 90 | 3         |
++----+-----------+
+Output: 
++-----------+----------+
+| unique_id | name     |
++-----------+----------+
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
++-----------+----------+
+Explanation: 
+Alice and Bob do not have a unique ID, We will show null instead.
+The unique ID of Meir is 2.
+The unique ID of Winston is 3.
+The unique ID of Jonathan is 1.
+*/
+
+-- code & explanations
+select b.unique_id, a.name -- select column name
+from Employees as a -- from table name 1 as a main table
+left join EmployeeUNI as b on b.id = a.id -- left join with table 2 to get the unique_id. since table 2 is not the primary table, if still can show all the unique name from primary table even there is no unique id in the table 2. So if you want to show only data that available on table 1 & 2, you can use inner join
