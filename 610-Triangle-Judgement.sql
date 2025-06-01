ff63dd78233c53ef375030118702911c3782f0fb
Elok Mutiaraningtyas <elokmutiaraningtyas@gmail.com>
2025-06-01 21:06:08 +0700

    Create 610-Triangle-Judgement.sql

---
+++ 610-Triangle-Judgement.sql
@@ -0,0 +1,53 @@
+/*
+610. Triangle Judgement
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+
+*/
+
+-- solution & explanation
+SELECT *,
+    CASE WHEN x+y > z 
+        AND y+z > x
+        AND z+x > y 
+        THEN 'Yes' -- the rule for Triangle line is: Lenth of one line is less than sum of lenth 2 others lines
+        ELSE 'No'
+        END as triangle
+FROM Triangle -- from table name
+
