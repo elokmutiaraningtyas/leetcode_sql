1326c86f80492ef4a0c71729b733212f6e4e48fa
Elok Mutiaraningtyas <elokmutiaraningtyas@gmail.com>
2025-06-01 20:08:46 +0700

    Create 1070-Product-Sales-Analysis-III.sql

---
+++ 1070-Product-Sales-Analysis-III.sql
@@ -0,0 +1,65 @@
+/*
+1070. Product Sales Analysis III
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row records a sale of a product in a given year.
+A product may have multiple sales entries in the same year.
+Note that the per-unit price.
+
+Write a solution to find all sales that occurred in the first year each product was sold.
+
+For each product_id, identify the earliest year it appears in the Sales table.
+
+Return all sales entries for that product in that year.
+
+Return a table with the following columns: product_id, first_year, quantity, and price.
+Return the result in any order.
+
+ 
+
+Example 1:
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Output: 
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+
+*/
+
+-- solution and explanation
+select b.product_id, 
+       b.first_year, 
+       a.quantity,
+       a.price
+  -- select prefered column
+from (select product_id, 
+      min(year) as first_year
+      from Sales
+      group by product_id) as b,  -- create subquery to get the first transaction of each product_id using MIN()
+      Sales as a -- get the other column from original table
+where a.product_id = b.product_id -- you can connect 2 table using WHERE id = id and year = year
+and a.year = b.first_year
