c87a2e131f3dd2298f2fadec3f888220231863cf
Elok Mutiaraningtyas <elokmutiaraningtyas@gmail.com>
2025-06-10 22:18:57 +0700

    Create 1341-Movie-Rating.sql

---
+++ 1341-Movie-Rating.sql
@@ -0,0 +1,117 @@
+/*
+1341. Movie Rating
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+SQL Schema
+Pandas Schema
+Table: Movies
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| movie_id      | int     |
+| title         | varchar |
++---------------+---------+
+movie_id is the primary key (column with unique values) for this table.
+title is the name of the movie.
+ 
+
+Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| name          | varchar |
++---------------+---------+
+user_id is the primary key (column with unique values) for this table.
+The column 'name' has unique values.
+Table: MovieRating
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| movie_id      | int     |
+| user_id       | int     |
+| rating        | int     |
+| created_at    | date    |
++---------------+---------+
+(movie_id, user_id) is the primary key (column with unique values) for this table.
+This table contains the rating of a movie by a user in their review.
+created_at is the user's review date. 
+ 
+
+Write a solution to:
+
+Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Movies table:
++-------------+--------------+
+| movie_id    |  title       |
++-------------+--------------+
+| 1           | Avengers     |
+| 2           | Frozen 2     |
+| 3           | Joker        |
++-------------+--------------+
+Users table:
++-------------+--------------+
+| user_id     |  name        |
++-------------+--------------+
+| 1           | Daniel       |
+| 2           | Monica       |
+| 3           | Maria        |
+| 4           | James        |
++-------------+--------------+
+MovieRating table:
++-------------+--------------+--------------+-------------+
+| movie_id    | user_id      | rating       | created_at  |
++-------------+--------------+--------------+-------------+
+| 1           | 1            | 3            | 2020-01-12  |
+| 1           | 2            | 4            | 2020-02-11  |
+| 1           | 3            | 2            | 2020-02-12  |
+| 1           | 4            | 1            | 2020-01-01  |
+| 2           | 1            | 5            | 2020-02-17  | 
+| 2           | 2            | 2            | 2020-02-01  | 
+| 2           | 3            | 2            | 2020-03-01  |
+| 3           | 1            | 3            | 2020-02-22  | 
+| 3           | 2            | 4            | 2020-02-25  | 
++-------------+--------------+--------------+-------------+
+Output: 
++--------------+
+| results      |
++--------------+
+| Daniel       |
+| Frozen 2     |
++--------------+
+Explanation: 
+Daniel and Monica have rated 3 movies ("Avengers", "Frozen 2" and "Joker") but Daniel is smaller lexicographically.
+Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smaller lexicographically.
+*/
+
+-- solution & explanation
+(SELECT a.name as results
+FROM Users as a
+LEFT JOIN MovieRating as b on b.user_id = a.user_id -- you can replace on b.user_id = a.user_id with "USING(user_id)"
+GROUP BY 1
+ORDER by count(b.rating) desc, 1 asc
+LIMIT 1)  -- limit 1 bcs we only take the highest one
+
+UNION ALL -- since the value of the result is 2 different calculation, one is who is the person who in AVG give the highest rating, and second row is whats movie with the best ratings in avg
+
+(SELECT a.title as results
+FROM Movies as a
+LEFT JOIN MovieRating as b on b.movie_id = a.movie_id -- you can replace on b.user_id = a.user_id with "USING(user_id)"
+WHERE month(created_at) = 2 AND year(created_at) = 2020
+GROUP BY 1
+ORDER by avg(b.rating) desc, 1 asc
+LIMIT 1) -- limit 1 bcs we only take the highest one
